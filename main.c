12bec41c0335fdb8367ce387b0be4c3fe3149bde
Ahmed Moustafa <geekahmed1@gmail.com>
2021-05-06 23:15:14 +0200

    Main Loop Basic Structure

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
+#include <malloc.h>
 
 #define EXIT_SUCCESS 0
 
 void main_loop_interpreter(void);
+char * read_line();
+char ** split_line(char *);
+int execute(char **);
+
 int main(int argc, char **argv) {
     // Load config files.
 
     // Main command loop interpreter.
-
+    main_loop_interpreter();
     // Pre-termination cleanup.
     return EXIT_SUCCESS;
 }
-// void main_loop_interpreter(void)
+void main_loop_interpreter(void){
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        line = read_line();
+        args = split_line(line);
+        status = execute(args);
+        free(line);
+        free(args);
+    } while (status);
+}
 
